74c3abcfd0dfa4359ea396eefe5e80391ab52f5e
raquel <raquelanaroman@gmail.com>
2021-05-03 14:47:56 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(nameArray, event){
+    let thankYouCards = [];
+    for (let i = 0; i <nameArray.length; i++){
+        thankYouCards.push(`Thank you, ${nameArray[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouCards;
+}
+
+function countDown(n){
+    while (n > 0){
+        console.log(n);
+        n -= 1;
+      }
+    console.log(n);
+}
